45220cec52f8d4c17d70428c6839ba783aeb7d00
edit8080 <tlfla01@gmail.com>
2021-02-22 14:06:34 +0900

    Can change line color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#js-canvas");
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll(".js-color");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -29,3 +30,16 @@ function onMouseMove(event){
     }
 }
 
+function changeColor(event){
+    const color =  event.target.style.backgroundColor
+    ctx.strokeStyle = color;
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
+    canvas.addEventListener("mouseleave",stopPainting);
+}
+
+Array.from(colors).forEach(color=>color.addEventListener("click",changeColor));
