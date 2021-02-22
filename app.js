c763859e769c29857764b27c15ae212284dcb4b5
edit8080 <tlfla01@gmail.com>
2021-02-22 14:04:51 +0900

    Created line on canvas

---
+++ app.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("#js-canvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+function startPainting(){
+    painting = true;
+}
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }
+    else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
